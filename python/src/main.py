736ddf5b876024b8700d4ebf4f759da1d58de6d3
CosmicGate <jackie.sechrist.3@gmail.com>
2024-01-19 09:28:02 -0600

    Creates contact dictionary

--- python/src/main.py
+++ python/src/main.py
@@ -1,6 +1,10 @@
 import contacts
 
-addressbook = []
+addressbook = [ 
+  { 'name': 'Bruce Wayne', 'phone': '555-123-4567', 'email': 'bruce@wayne.com' },
+  { 'name': 'Clark Kent', 'phone': '555-222-3333', 'email': 'clark@dailyplanet.com' },
+  { 'name': 'Diana Prince', 'phone': '555-444-5555', 'email': 'diana@amazon.com' }
+  ]
 
 def menu():
   pass
